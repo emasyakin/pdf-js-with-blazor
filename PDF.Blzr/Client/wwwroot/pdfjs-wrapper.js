7ace7a4b39d7ec84901b44bc8acf1ae79ece1636
Evgen Masiakin <mea88mts@gmail.com>
2023-03-30 23:08:14 -0400

    Add support for pdf scroll

--- PDF.Blzr/Client/wwwroot/pdfjs-wrapper.js
+++ PDF.Blzr/Client/wwwroot/pdfjs-wrapper.js
@@ -1,141 +1,60 @@
-// pdfjs-wrapper.js
-
-function recreateNode(el, withChildren) {
-  if (withChildren) {
-    el.parentNode.replaceChild(el.cloneNode(true), el);
-  } else {
-    const newEl = el.cloneNode(false);
-    while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
-    el.parentNode.replaceChild(newEl, el);
-  }
-}
-
 const pdfJSViewerFunctions = {
   state: {
     pdfDoc: null,
     pageNum: 1,
-    pageRendering: false,
-    pageNumPending: null,
-    scale: 0.8,
-    canvas: null,
-    ctx: null,
-    currentPage: null,
-    pdfJsLib: null
+    scale: 1.5,
+    canvasWrapper: null,
+    pdfjsLib: null,
   },
 
-  init(element, url) {
+  async init(canvasWrapper, url) {
     this.setupPdfJsLib();
-    this.setupCanvas(element);
-    this.loadDocument(url);
-  },
-
-  setupCanvas(element) {
-    this.state.canvas = element;
-    this.state.ctx = this.state.canvas.getContext('2d');
-
-    this.state.canvas.style.overflowY = 'scroll';
-    this.state.canvas.addEventListener('scroll', this.throttle(this.onScroll.bind(this), 200));
+    this.setupCanvasWrapper(canvasWrapper);
+    await this.loadDocument(url);
+    await this.renderPages();
   },
 
   setupPdfJsLib() {
-    pdfJSViewerFunctions.state.pdfjsLib = window['pdfjs-dist/build/pdf'];
-    pdfJSViewerFunctions.state.pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    this.state.pdfjsLib = window['pdfjs-dist/build/pdf'];
+    this.state.pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
   },
 
-  loadDocument(url) {
-    pdfJSViewerFunctions.state.pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
-      if (this.state.currentPage) {
-        this.state.currentPage.cleanup();
-        this.state.pageRendering = false;
-      }
-
-      if (this.state.pdfDoc) {
-        this.state.pdfDoc.destroy();
-      }
-
-      if (this.state.ctx) {
-        this.state.ctx.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
-        this.state.ctx.beginPath();
-      }
-
-      this.state.pdfDoc = pdfDoc_;
-      document.getElementById('page_count').textContent = this.state.pdfDoc.numPages;
-
-      this.renderPage(this.state.pageNum);
-    });
+  setupCanvasWrapper(canvasWrapper) {
+    this.state.canvasWrapper = canvasWrapper;
+    this.state.canvasWrapper.style.display = 'flex';
+    this.state.canvasWrapper.style.flexWrap = 'wrap';
   },
 
-  renderPage(num) {
-    this.state.pageRendering = true;
-
-    this.state.pdfDoc.getPage(num).then((page) => {
-      const viewport = page.getViewport({ scale: this.state.scale });
-      this.state.canvas.height = viewport.height;
-      this.state.canvas.width = viewport.width;
-
-      const renderContext = {
-        canvasContext: this.state.ctx,
-        viewport: viewport,
-      };
-
-      const renderTask = page.render(renderContext);
-      this.state.currentPage = page;
-
-      renderTask.promise.then(() => {
-        this.state.pageRendering = false;
-        if (this.state.pageNumPending !== null) {
-          this.renderPage(this.state.pageNumPending);
-          this.state.pageNumPending = null;
-        }
-      });
-    });
-
-    document.getElementById('page_num').textContent = num;
+  async loadDocument(url) {
+    this.state.pdfDoc = await this.state.pdfjsLib.getDocument(url).promise;
   },
 
-  queueRenderPage(num) {
-    if (this.state.pageRendering) {
-      this.state.pageNumPending = num;
-    } else {
-      this.renderPage(num);
+  async renderPages() {
+    for (let i = 1; i <= this.state.pdfDoc.numPages; i++) {
+      await this.renderPage(i);
     }
   },
 
-  onPrevPage() {
-    if (this.state.pageNum <= 1) {
-      return;
-    }
-    this.state.pageNum--;
-    this.queueRenderPage(this.state.pageNum);
-  },
+  async renderPage(num) {
+    const canvas = document.createElement('canvas');
+    this.state.canvasWrapper.appendChild(canvas);
 
-  onNextPage() {
-    if (this.state.pageNum >= this.state.pdfDoc.numPages) {
-      return;
-    }
-    this.state.pageNum++;
-    this.queueRenderPage(this.state.pageNum);
-  },
+    const page = await this.state.pdfDoc.getPage(num);
+    const viewport = page.getViewport({ scale: this.state.scale });
+    const canvasWidth = viewport.width;
+    const canvasHeight = viewport.height;
 
-  onScroll() {
-    if (this.state.canvas.scrollTop + this.state.canvas.clientHeight >= this.state.canvas.scrollHeight) {
-      this.onNextPage();
-    }
-    else if (this.state.canvas.scrollTop === 0) {
-      this.onPrevPage();
-    }
-  },
+    canvas.height = canvasHeight * window.devicePixelRatio;
+    canvas.width = canvasWidth * window.devicePixelRatio;
 
-  throttle(func, delay) {
-    let lastCall = 0;
-    return function (...args) {
-      const now = new Date().getTime();
-      if (now - lastCall < delay) {
-        return;
-      }
-      lastCall = now;
-      return func.apply(this, args);
+    const renderContext = {
+      canvasContext: canvas.getContext('2d'),
+      viewport: viewport,
     };
+
+    canvas.getContext('2d').scale(window.devicePixelRatio, window.devicePixelRatio);
+
+    await page.render(renderContext).promise;
   },
 };
 
