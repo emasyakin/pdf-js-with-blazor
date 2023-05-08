e2a01a957b91f7feee96a919306dc52d51f614a6
Evgen Masiakin <mea88mts@gmail.com>
2023-05-08 18:21:57 -0400

    Fix problem with reloading

--- PDF.Blzr/Client/wwwroot/service-worker.published.js
+++ PDF.Blzr/Client/wwwroot/service-worker.published.js
@@ -33,9 +33,15 @@ async function onInstall(event) {
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+
+    // This change forces the browser to update the application if it has changed.
+    // When this happens any page reload triggers refresh of the application.
+    self.skipWaiting();
 }
 
 async function onActivate(event) {
+    // Activation happens when application sources have been downloaded and available (because we use skipWaiting above)
+    // At this point we can show a prompt to reload the page.
     console.info('Service worker: Activate');
 
     // Delete unused caches
